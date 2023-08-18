514130059a1470abac0b3ebd8a4ca8c292b68f3c
Daparamo <Daparamo@hotmail.com>
2023-08-18 01:47:37 -0500

    ok

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'Tareas',
   appName: 'Tareas',
   webDir: 'www',
   server: {
